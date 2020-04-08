61e02875321c920a7b6c309dba2ad23bcb7e0602
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-04-09 00:40:58 +0600

    add unit test project

---
+++ 02 module/02_Module/XUnitTestFileVisitor/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace XUnitTestFileVisitor
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           
+        }
+    }
+}
