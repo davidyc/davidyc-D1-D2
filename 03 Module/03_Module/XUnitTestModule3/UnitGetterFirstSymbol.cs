8e60969c714afbb1ea749d0f8babfb8ba66c49a6
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-04-15 19:08:17 +0600

    Module 3 fix unit tests name

--- 03 Module/03_Module/XUnitTestModule3/UnitGetterFirstSymbol.cs
+++ 03 Module/03_Module/XUnitTestModule3/UnitGetterFirstSymbol.cs
@@ -7,16 +7,35 @@ namespace XUnitTestModule3
     public class UnitGetterFirstSymbol
     {
         [Theory]
-        [InlineData("Hello", "H")]           
+        [InlineData("Hello", "H")]
         [InlineData("luck", "l")]
         [InlineData("1234567", "1")]
-        [InlineData("", "Input string is null or empty")]
-        [InlineData(null, "Input string is null or empty")]
-
-        public void TestGetFirstSymbol(string value, string expected)
+        public void GetFirstSymbol_InputString_FirstSymbol(string value, string expected)
         {
-            var actual = GetterFirstSymbol.GetFirstSymbol(value);  
+            var actual = GetterFirstSymbol.GetFirstSymbol(value);
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetFirstSymbol_EmptyString_StringNullOrEmptyException()
+        {
+            //Arrange
+            var inputString = String.Empty;
+            // Act
+            Action actual = () => GetterFirstSymbol.GetFirstSymbol(inputString);
+            // Assert
+            Assert.Throws<StringNullOrEmptyException>(actual);
+        }
+
+        [Fact]
+        public void GetFirstSymbol_Null_StringNullOrEmptyException()
+        {
+            //Arrange
+            string inputString = null;
+            // Act
+            Action actual = () => GetterFirstSymbol.GetFirstSymbol(inputString);
+            // Assert
+            Assert.Throws<StringNullOrEmptyException>(actual);
         }
     }
 }
--- 03 Module/03_Module/XUnitTestModule3/UnitIntParser.cs
+++ 03 Module/03_Module/XUnitTestModule3/UnitIntParser.cs
@@ -12,66 +12,53 @@ namespace XUnitTestModule3
         [InlineData("257", 257)]
         [InlineData("-159", -159)]
         [InlineData("1234567", 1234567)]
-        public void TestParseInt(string value, int expected)
+        public void ParseInt_StringNumber_IntNumber(string value, int expected)
         {
             var actual = IntParser.ParseInt(value);
             Assert.Equal(expected, actual);
         }
 
-        //пойдет ли такой тест на проверку эксепшена
         [Fact]
-        public void TestIsntFormatExceptio()
+        public void ParseInt_String12Fr444_IntFormatException()
         {
             //Arrange
-            var value = "12Fr444";
-            var expected = new IntFormatException("String have invalid symbol");
-            IntFormatException actual = null;
-
+            var inputString = "12Fr444";             
             // Act
-            try
-            {
-                var numint = IntParser.ParseInt(value);
-            }
-            catch(IntFormatException e)
-            {
-                // Assert          
-                actual = e;                
-            }
-            Assert.IsType(expected.GetType(), actual);
+            Action actual = () => IntParser.ParseInt(inputString);
+            // Assert
+            Assert.Throws<IntFormatException>(actual);              
         }
                 
         [Theory]
         [InlineData("2147483648")]
         [InlineData("-2147483649")]
-        public void TestIntSizeException(string number)
+        public void ParseInt_ToLargeInt_IntSizeException(string number)
         {
             //Arrange
-            var expected = new IntSizeException("Too large or too small to int");
-            IntSizeException actual = null;
-
+            var inputString = number;
             // Act
-            try
-            {
-                var numint = IntParser.ParseInt(number);               
-            }
-            catch (IntSizeException e)
-            {
-                // Assert           
-                actual = e;                
-            }
-
-            Assert.IsType(expected.GetType(), actual);
+            Action actual = () => IntParser.ParseInt(inputString);
+            // Assert
+            Assert.Throws<IntSizeException>(actual);
         }
 
         [Theory]
         [InlineData("10", true)]       
         [InlineData("257", true)]
         [InlineData("-159", true)]
-        [InlineData("1234567", true)]
+        [InlineData("1234567", true)]        
+        public void TryParseInt_NumberString_True(string value, bool expected)
+        {
+            int number;
+            var actual = IntParser.TryParseInt(value, out number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
         [InlineData("2ddd57", false)]
         [InlineData("2147483648", false)]
         [InlineData("-2147483649", false)]
-        public void TestTryParseIntTrueFalse(string value, bool expected)
+        public void TryParseInt_IncorrectInt_False(string value, bool expected)
         {
             int number;
             var actual = IntParser.TryParseInt(value, out number);
@@ -83,7 +70,7 @@ namespace XUnitTestModule3
         [InlineData("257", 257)]
         [InlineData("-159", -159)]
         [InlineData("1234567", 1234567)]
-        public void TestTryParseInt(string value, int expected)
+        public void TryParseInt_NumberString_IntNumber(string value, int expected)
         {
             int actual;
             var succseful = IntParser.TryParseInt(value, out actual);
