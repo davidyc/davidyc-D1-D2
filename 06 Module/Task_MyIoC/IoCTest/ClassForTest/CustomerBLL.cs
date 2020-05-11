f962ba1fae98d4a6185725d9db7555fa5d6aa3f6
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-05-11 20:46:29 +0600

    Module 5 add prop

--- 06 Module/Task_MyIoC/IoCTest/ClassForTest/CustomerBLL.cs
+++ 06 Module/Task_MyIoC/IoCTest/ClassForTest/CustomerBLL.cs
@@ -6,7 +6,13 @@ namespace IoCTest.ClassForTest
     public class CustomerBLL
     {
         public CustomerBLL(ICustomerDAL dal, Logger logger)
-        { }
+        {
+            Logger = logger;
+            ICustomerDAL = dal;
+        }
+
+        public ICustomerDAL ICustomerDAL { get; set; }
+        public Logger Logger { get; set; }
     }
 
 }
