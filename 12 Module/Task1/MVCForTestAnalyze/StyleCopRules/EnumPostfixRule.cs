523b1e32d58be088298af6bf7d3062ad9b8906e6
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-06-20 22:05:27 +0600

    add task 1 module 12

---
+++ 12 Module/Task1/MVCForTestAnalyze/StyleCopRules/EnumPostfixRule.cs
@@ -0,0 +1,34 @@
+﻿using StyleCop;
+using StyleCop.CSharp;
+
+namespace StyleCopRules
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class EnumPostfixRule : SourceAnalyzer
+    {
+        const string ruleName = "EnumPostfixRule";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument csDocument = (CsDocument)document;
+            csDocument.WalkDocument(
+                new CodeWalkerElementVisitor<object>(this.VisitElement));
+        }
+
+        private bool VisitElement(CsElement element, CsElement parentElement, object context)
+        {
+            var enumElement = element as Enum;
+            if (enumElement != null)
+            {
+                if (!enumElement.Name.EndsWith("Enum", System.StringComparison.Ordinal))
+                {
+                    this.AddViolation(element, ruleName);
+                }
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
