035eabb2f257b222d8dcffa840241d142d03da22
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-06-22 23:09:08 +0600

    add struct to module 13

---
+++ 13 Module/Module_13/ConsoleDownLoaderClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using SiteDownloader;
+using SiteDownloader.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDownLoaderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sd = new SiteContentDownloader(new Saver());
+            sd.LoadFromURL("https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%B9%D1%82");
+            Console.WriteLine();
+        }
+    }
+
+    public class Saver : IContentSaver
+    {
+        public void SaveFile(Uri uri, Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveHtmlDocument(Uri uri, string name, Stream documentStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 13 Module/Module_13/SiteDownloader/SiteDownloader.cs
@@ -0,0 +1,83 @@
+﻿using SiteDownloader.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteDownloader
+{
+    public class SiteContentDownloader : ISiteContentDownloader
+    {
+        private readonly int _maxDeepLevel;
+        private const string htmlDocumentMediaType = "text/html";
+        private readonly IContentSaver contentSaver;
+
+        public SiteContentDownloader(IContentSaver contentSaver, int maxDeepLevel = 0)
+        {
+            if(maxDeepLevel < 0)
+            {
+                new ArgumentException("Bad argument. Deep kevel can not be less zero");
+            }
+            _maxDeepLevel = maxDeepLevel;
+        }
+
+        public void LoadFromURL(string URL)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(URL);
+                GetPageByURL(httpClient, httpClient.BaseAddress, 0);
+            }
+        }
+
+        private void GetPageByURL(HttpClient httpClient, Uri uri, int level)
+        {
+            if (IsMaxDeepLevel(level))
+            {
+                return;
+            }
+            var header = GetHeaderWithSuccessStatusCode(httpClient, uri);
+            if (header == null)
+            {
+                return;
+            }
+            if(header.Content.Headers.ContentType.MediaType == htmlDocumentMediaType)
+            {
+                Console.WriteLine("DownLoadPage");
+                GetPageByURL(httpClient, uri, level + 1);
+            }
+            else
+            {
+                Console.WriteLine("DownLoadFile");
+            }
+        }
+
+        private bool IsMaxDeepLevel(int level)
+        {
+            return level > _maxDeepLevel ? true : false;
+        }
+
+        private HttpResponseMessage GetHeaderWithSuccessStatusCode(HttpClient httpClient, Uri uri)
+        {
+            HttpResponseMessage head = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, uri)).Result;
+            if (!head.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            return head;
+        }
+
+        private void SaveContentFile(HttpClient httpClient, Uri uri)
+        {
+            var response = httpClient.GetAsync(uri).Result;           
+            contentSaver.SaveFile(uri, response.Content.ReadAsStreamAsync().Result);
+        }
+
+
+
+
+    }
+}
