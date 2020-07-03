664bb34aeebd38e6acbc1e955ca75cdfd8797cb1
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-07-03 12:00:34 +0600

    No bat

--- 14 Module/Task/MvcMusicStore/logs/errorsReportScript.sql
+++ 14 Module/Task/MvcMusicStore/logs/errorsReportScript.sql
@@ -1,5 +1,1 @@
-SELECT 
-    Text AS Errors
-INTO ReportError.csv
-FROM 2020-07-01_info.log
-WHERE Text LIKE '%ERROR%'
+Logparser " SELECT Text AS Errors INTO ReportError.csv FROM 2020-07-01_info.log WHERE Text LIKE '%ERROR%'" -o datagrid
--- 14 Module/Task/MvcMusicStore/logs/totalReportScript.sql
+++ 14 Module/Task/MvcMusicStore/logs/totalReportScript.sql
@@ -1,13 +1,1 @@
-SELECT
-TRIM(SUBSTR(text, 25, 5)) AS [Log_levels],
-COUNT([Index]) AS [Total_Count]
-INTO ReportCount.CSV
-FROM 2020-07-02_info.log
-WHERE (CASE TRIM(SUBSTR(text, 25, 5))
-         WHEN 'ERROR' THEN 1
-         WHEN 'DEBUG' THEN 1
-         WHEN 'INFO' THEN 1
-         ELSE 2
-         END = 1)
- GROUP BY TRIM(SUBSTR(text, 25, 5))
- 
+Logparser "SELECT TRIM(SUBSTR(text, 25, 5)) AS [Log_levels], COUNT([Index]) AS [Total_Count] INTO ReportCount.CSV FROM 2020-07-01_info.log WHERE (CASE TRIM(SUBSTR(text, 25, 5)) WHEN 'ERROR' THEN 1 WHEN 'DEBUG' THEN 1 WHEN 'INFO' THEN 1 ELSE 2 END = 1) GROUP BY TRIM(SUBSTR(text, 25, 5))" -o datagrid
